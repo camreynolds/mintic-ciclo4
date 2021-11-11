74d93e88fcd7222272da4383d2c71375b3f1c1bb
Carlos Muñoz <camr.personal@gmail.com>
2021-11-10 22:17:15 -0500

    apipeliculas_53861/app.js. Updated.

--- apipeliculas_53861/app.js
+++ apipeliculas_53861/app.js
@@ -1,23 +1,21 @@
 /**
  * @param Requerido: Importar los módulos.
  */
-const express = require('express');
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const controladorPeliculas = require('./api/peliculas/controller');
+const controladorUsuarios = require('./api/usuarios/controller');
 
 // Se crea una instancia de "express", he iniciar la configuración.
 const app   = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true})); // Para formularios. Convierte el form-data en json 
 const port  = 3002;
 
-// Se inicia las rutas.
-app.get("/prueba", function(request,response){
-    // Procesar la petición.
-    let nombres = "Carlos Muñoz";
-    // Enviar la respuesta.
-    response.send(nombres)
-});
 
-app.get("/misiontic", function(request,response){
-    response.send("Mi primer API 53861")
-});
+// Se inicia las rutas.
+app.use("/api/peliculas",controladorPeliculas);
+app.use("/api/usuarios",controladorUsuarios);
 
 // Configurar en dónde el API va a estar escuchando las peticiones.
 app.listen(port,function(){

7900d3447407efcdb5fe7baacac6323ce681f5a9
Carlos Muñoz <camr.personal@gmail.com>
2021-11-12 22:11:21 -0500

    Actualizado.

--- apipeliculas_53861/api/peliculas/model.js
+++ apipeliculas_53861/api/peliculas/model.js
@@ -1,9 +1,20 @@
+/**
+ *      Modelo -> Gestiona la base de datos.
+ *          -> Realiza consulta de los datos a petición de los servicios
+  *             -> Lectura -> Selecciona
+ *              -> Escribe.
+ *              -> Actualiza.
+ *              -> Elimina.
+ *          -> Envía los resultados de la acción al controlador.
+ */
+
 const basedatos = require('../../database/connection');
+const objectId  = require('mongodb').ObjectId;
 
-function findAll(){
-    let conexion = basedatos.obtenerConexion();
+function buscarTodos(){
+    let db = basedatos.obtenerConexion();
 
-    return conexion.collection("peliculas").find({}).toArray()
+    return db.collection("peliculas").find({}).toArray()
         .then(function(data){
             return data;
         })
@@ -12,4 +23,31 @@ function findAll(){
         });
 };
 
-module.exports.findAll = findAll;
+function bucarPorId(id){
+    let db = basedatos.obtenerConexion();
+
+    return db.collection("peliculas").findOne({"_id": objectId(id) })
+        .then(function(data){
+            return data;
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+};
+
+function bucarPorTitulo(nombre){
+    let db = basedatos.obtenerConexion();
+
+    return db.collection("peliculas").find({"titulo": new RegExp(nombre,"i") }).toArray()
+        .then(function(data){
+            return data;
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+};
+
+
+module.exports.buscarTodos      = buscarTodos;
+module.exports.bucarPorId       = bucarPorId;
+module.exports.bucarPorTitulo   = bucarPorTitulo;

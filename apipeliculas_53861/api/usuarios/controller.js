75d4b839098a2f1488de07367b9f60d4c12eebc5
Carlos Muñoz <camr.personal@gmail.com>
2021-11-17 22:07:05 -0500

    Actualizado.

--- apipeliculas_53861/api/usuarios/controller.js
+++ apipeliculas_53861/api/usuarios/controller.js
@@ -1,15 +1,38 @@
 const express               = require('express');
 const controladorUsuarios   = express.Router();
+const servicioUsuarios      = require("./service");
 
 /**
  *      GET: Login.
  *      GET: Obtener usuario por id.
  */
 
- controladorUsuarios.post("/iniciarsesion",function(req,res){
+ controladorUsuarios.get("/iniciarsesion",async function(req,res){
     // Capturar los datos, y enviarlos al servicio.
     let datos = req.query;
-    res.send("Los datos del usuario son: " + datos.usuario + " - " + datos.contrasena);
+    let resultado = await servicioUsuarios.iniciarSesion(datos);
+    res.send(resultado);
 });
+
+/**
+ *      nuevoUsuario = {
+ *          "nombre":"xxxx",
+ *          "usuario":"xxxx",
+ *          "clave":"xxxx", -> Encriptar
+ *          "roles":["A","B"],
+ * 
+ *      }
+ */
+
+/**
+ *      Crear usuario.
+ */
+controladorUsuarios.post("/crearUsuario", async function(req,res){
+    let nuevoUsuario    = req.body;
+    let resultado       = await servicioUsuarios.crearUsuario(nuevoUsuario);
+    res.send(resultado);
+});
+
+
 
 module.exports = controladorUsuarios;

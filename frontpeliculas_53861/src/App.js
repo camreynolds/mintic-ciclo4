b42b3474ec4f453648cbda93da187aafd7032f57
Carlos Muñoz <camr.personal@gmail.com>
2021-11-23 22:03:35 -0500

    Actualizado.

--- frontpeliculas_53861/src/App.js
+++ frontpeliculas_53861/src/App.js
@@ -1,23 +1,9 @@
-import ComponenteClase from "./ComponenteClase";
-import MiPrimerComponente from "./MiPrimerComponente";
-import MiPrimerComponenteArray from "./MiPrimerComponenteArray";
+import ResultadosBusqueda from "./app/paginas/ResultadosBusqueda";
 
 function App(){
-
-  let nombre    = "Jimmy";
-  let apellido  = "James";
-  let personas  = [
-    {"nombre":"Jimmy","apellido":"James"},
-    {"nombre":"Kevin","apellido":"Jones"},
-    {"nombre":"Robert","apellido":"McKey"},
-  ];
-
   return(
     <>
-      <MiPrimerComponente nombre={nombre} apellido={apellido}/>
-      <br/><br/>
-      <h1>Datos desde un Array.</h1>
-      <MiPrimerComponenteArray personas={personas} />
+      <ResultadosBusqueda/>
     </>
   )
 };

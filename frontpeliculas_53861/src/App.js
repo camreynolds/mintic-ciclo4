3d45fdc3a56055c0fee8cd018de7a6636d9ba9fc
Carlos Muñoz <camr.personal@gmail.com>
2022-01-04 14:50:21 -0500

    Actualizado.

--- frontpeliculas_53861/src/App.js
+++ frontpeliculas_53861/src/App.js
@@ -1,25 +1,10 @@
-import MiPrimerComponente from "./MiPrimerComponente";
-import MiPrimerComponenteArray from "./MiPrimerComponenteArray";
+import ResultadosBusqueda from "./app/paginas/ResultadosBusqueda";
 
 function App() {
-  let nombre = "Jason";
-  let apellido = "Drake";
-  let personas = [
-    {"nombre":"Joan","apellido":"Smith"},
-    {"nombre":"Kurt","apellido":"Cobain"},
-    {"nombre":"Chris","apellido":"Churchill"}
-  ];
-
+ 
   return (
     <>
-      <h1>Mis Primeros Paso en React.</h1>
-      <MiPrimerComponente nombre={nombre} apellido={apellido}/>
-      <MiPrimerComponente nombre="Drako" apellido="Malfoy"/>
-      <MiPrimerComponente nombre="Harry" apellido="Potter"/>
-      <MiPrimerComponente nombre="Lucas" apellido="Smith"/>
-      <br></br>
-      <h1>Datos desde un Array</h1>
-      <MiPrimerComponenteArray personas={personas}/>
+      <ResultadosBusqueda/>  
     </>
   );
 }

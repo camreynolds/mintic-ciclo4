cf04a3f2b88a1f8bfb62e7cd3dd603801ecaae3b
Carlos Muñoz <camr.personal@gmail.com>
2022-01-04 14:50:40 -0500

    Creado.

---
+++ frontpeliculas_53861/src/app/componentes/Resultado.js
@@ -0,0 +1,23 @@
+export default function Resultado(props){    
+    return(
+        <>
+            <div className="dv-pelicula">
+                <div className="dv-poster">
+                    <img src="https://pbs.twimg.com/media/DwSC9iNUwAAW30x?format=jpg&name=medium"/>
+                </div>
+                <div>
+                    <h3>TÍTULO</h3>
+                </div>
+                <div>
+                    <p>SINOPSIS</p>
+                </div>
+                <div>
+                    <span>
+                        Rating
+                        <i></i>
+                    </span>
+                </div>
+            </div>
+        </>
+    );
+};
